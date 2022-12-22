86ec1577d53e572d90371790842325cf5c55edcd
YalokinK <116567421+YalokinK@users.noreply.github.com>
2022-12-22 20:14:29 +0000

    Fail code task added

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа для решения этой задачи
+Console.WriteLine("Введите слова или числа через запятую: ");
+string strText = Console.ReadLine();
+string [] substring = strText.Split(',');
+int count = 3;
+Console.WriteLine("Выделенные строки <= 3 символов: ");
+for (int i=0; i<substring.Length; i++)
+{
+    if (substring[i].Length <= count)
+    {
+        Console.Write($"[{substring[i]}] ");
+    }
+}
+
